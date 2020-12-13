c3c876113948fe64108cf19ef73b5af48d94e4c5
Fawad Ali Siddiqi <fawad.siddiqi@systemsltd.com>
2020-12-13 14:23:21 +0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+<form class='jotform-form' action='https://submit.jotform.com/submit/203465705288057/' method='post' name='form_203465705288057' id='203465705288057' accept-charset='utf-8' autocomplete='on'>
+  <input type='hidden' name='formID' value='203465705288057' />
+  <input type='hidden' id='JWTContainer' value='' />
+  <input type='hidden' id='cardinalOrderNumber' value='' />
+  <div role='main' class='form-all'>
+    <ul class='form-section page-section'>
+      <li id='cid_1' class='form-input-wide' data-type='control_head'>
+        <div class='form-header-group  header-large'>
+          <div class='header-text httal htvam'>
+            <h1 id='header_1' class='form-header' data-component='header'>
+              LiveChat Demo
+            </h1>
+          </div>
+        </div>
+      </li>
+      <li class='form-line' data-type='control_email' id='id_5'>
+        <label class='form-label form-label-top form-label-auto' id='label_5' for='input_5'> Email </label>
+        <div id='cid_5' class='form-input-wide' data-layout='half'>
+          <span class='form-sub-label-container' >
+            <input type='email' id='input_5' name='q5_email' class='form-textbox validate[Email]'  size='310' value='' data-component='email' aria-labelledby='label_5 sublabel_input_5' />
+            <label class='form-sub-label' for='input_5' id='sublabel_input_5'  aria-hidden='false'> example@example.com </label>
+          </span>
+        </div>
+      </li>
+      <li class='form-line' data-type='control_button' id='id_2'>
+        <div id='cid_2' class='form-input-wide' data-layout='full'>
+          <div data-align='auto' class='form-buttons-wrapper form-buttons-auto   jsTest-button-wrapperField'>
+            <button id='input_2' type='submit' class='form-submit-button submit-button jf-form-buttons jsTest-submitField' data-component='button' data-content=''>
+              Submit
+            </button>
+          </div>
+        </div>
+      </li>
+      <li hidden>
+        Should be Empty:
+        <input type='text' name='website' value='' />
+      </li>
+    </ul>
+  </div>
+ 
+  <div class='formFooter-heightMask'>
+  </div>
+  <div class='formFooter f6'>
+    <div class='formFooter-wrapper formFooter-leftSide'>
+      <a href='https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=203465705288057&utm_content=jotform_logo&utm_campaign=powered_by_jotform_le' target='_blank' class='formFooter-logoLink'><img class='formFooter-logo' src='https://cdn.jotfor.ms/assets/img/logo/logo-new@1x.png' alt='Jotform Logo'></img></a>
+    </div>
+    <div class='formFooter-wrapper formFooter-rightSide'>
+      <span class='formFooter-text'>
+        Now create your own JotForm - It's free!
+      </span>
+      <a class='formFooter-button' href='https://www.jotform.com/?utm_source=formfooter&utm_medium=banner&utm_term=203465705288057&utm_content=jotform_button&utm_campaign=powered_by_jotform_le' target='_blank'>Create your own JotForm</a>
+    </div>
+  </div>
+</form>
+
+</header>
+     
     </div>
   );
 }
